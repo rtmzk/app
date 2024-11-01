d201b2a944c5031bfa2d92d8931b8a127cd1b342
rtmzk <13581854511@163.com>
2024-11-01 10:08:18 +0800

    init: first commit

---
+++ config.go
@@ -0,0 +1,82 @@
+// Copyright 2024 rtmzk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package app
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gosuri/uitable"
+	"github.com/rtmzk/component-base/utils/homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const configFlagName = "config"
+
+var cfgFile string
+
+// nolint: gochecknoinits
+func init() {
+	pflag.StringVarP(&cfgFile, "config", "c", cfgFile, "Read configuration from specified `FILE`, "+
+		"support JSON, TOML, YAML, HCL, or Java properties formats.")
+}
+
+// addConfigFlag adds flags for a specific server to the specified FlagSet
+// object.
+func addConfigFlag(basename string, fs *pflag.FlagSet) {
+	fs.AddFlag(pflag.Lookup(configFlagName))
+
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix(strings.Replace(strings.ToUpper(basename), "-", "_", -1))
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+
+	cobra.OnInitialize(func() {
+		if cfgFile != "" {
+			viper.SetConfigFile(cfgFile)
+		} else {
+			viper.AddConfigPath(".")
+
+			if names := strings.Split(basename, "-"); len(names) > 1 {
+				viper.AddConfigPath(filepath.Join(homedir.HomeDir(), "."+names[0]))
+				viper.AddConfigPath(filepath.Join("/etc", names[0]))
+			}
+
+			viper.SetConfigName(basename)
+		}
+
+		if err := viper.ReadInConfig(); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error: failed to read configuration file(%s): %v\n", cfgFile, err)
+			os.Exit(1)
+		}
+	})
+}
+
+func printConfig() {
+	if keys := viper.AllKeys(); len(keys) > 0 {
+		fmt.Printf("%v Configuration items:\n", progressMessage)
+		table := uitable.New()
+		table.Separator = " "
+		table.MaxColWidth = 80
+		table.RightAlign(0)
+		for _, k := range keys {
+			table.AddRow(fmt.Sprintf("%s:", k), viper.Get(k))
+		}
+		fmt.Printf("%v", table)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,46 @@
+module github.com/rtmzk/app
+
+go 1.23.0
+
+require (
+	github.com/fatih/color v1.17.0
+	github.com/gosuri/uitable v0.0.4
+	github.com/rtmzk/component-base v0.0.0-20241009063241-4439a47fb0fd
+	github.com/rtmzk/errors v0.0.0-20240930011710-2e5cd5295fe0
+	github.com/rtmzk/log v0.0.1
+	github.com/spf13/cobra v1.8.1
+	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.19.0
+)
+
+require (
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sagikazarmark/locafero v0.6.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/exp v0.0.0-20241004190924-225e2abe05e6 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog v1.0.0 // indirect
+)
